52deb748560904352e07e79787e61019011e2daa
Carsten Meininger <carsten.meininger@bahag.com>
2025-10-26 12:44:49 +0100

    fix: use correct and new appid

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	// Mit eindeutiger ID starten (fix für Preferences-Fehler)
 	os.Setenv("FYNE_APP_STORAGE", "/Users/carstenmeininger/go/src/backlog-manager/.fyne")
-	a := app.NewWithID("backlog-manager")
+	a := app.NewWithID("com.scramb.backlog-manager")
 	w := a.NewWindow("Backlog Manager")
 
 	// Persistierte Einstellungen aus Preferences lesen
